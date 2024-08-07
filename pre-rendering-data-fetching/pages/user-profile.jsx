c385c4f6340426ca0c48e9aff98fc80ee7ef977c
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-07 16:14:10 -0300

    Working with getServerSideProps

---
+++ pre-rendering-data-fetching/pages/user-profile.jsx
@@ -0,0 +1,16 @@
+function UserProfilePage(props) {
+  const { username } = props;
+  return <h1>{username}</h1>;
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  return {
+    props: {
+      username: "Jeremy",
+    },
+  };
+}

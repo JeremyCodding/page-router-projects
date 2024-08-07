cb8a22286cba5d8a671bde2e252565ba41f5c0c1
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-07 16:05:20 -0300

    Redirect to not found page

--- pre-rendering-data-fetching/pages/[pid].jsx
+++ pre-rendering-data-fetching/pages/[pid].jsx
@@ -15,17 +15,27 @@ function ProductDetailPage(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
+  if (!product) {
+    console.log("not found");
+    return { notFound: true };
+  }
   return {
     props: {
       loadedProduct: product,
@@ -34,8 +44,12 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const params = ids.map((id) => ({ params: { pid: id } }));
   return {
-    paths: [{ params: { pid: "p1" } }],
+    paths: params,
     fallback: true,
   };
 }
